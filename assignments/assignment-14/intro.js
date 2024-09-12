41fb14740567a643f2e3204563d093e672d15cef
Muhammad Eshpulat <eshpulat.dev@gmail.com>
2024-09-12 19:42:57 -0400

    Updat assignment-14

--- assignments/assignment-14/intro.js
+++ assignments/assignment-14/intro.js
@@ -37,12 +37,56 @@ Array.prototype.myRemove = function (value) {
 // generateFibonacci ------ 4 ----------
 
 function generateFibonacci(num) {
-    let result = 0;
-    for (let i = 0; i < num; i++) {
-        console.log(num[i]);
+    let result = [];
+    let a = 0;
+    let b = 1;
+
+    result.push(a);
+    result.push(b);
+
+    for (let i = 2; i < num; i++) {
+        let fibonacciNum = a + b;
+        result.push(fibonacciNum);
+
+        a = b;
+        b = fibonacciNum;
     }
     return result;
 }
 
 console.log(generateFibonacci(5)); // [0, 1, 1, 2, 3]
 console.log(generateFibonacci(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+
+// leastCommonMultiple -------- 5 ------ ??????????
+
+function leastCommonMultiple(num1, num2) {
+    let maxNum = num2;
+    let result = 0;
+
+    for (let i = 0; i < maxNum; i++) {
+        if (i % num1 === 0 && i % num2 === 0) {
+            maxNum += num2;
+        }
+    }
+    return maxNum;
+}
+
+// console.log(leastCommonMultiple(2, 3)); // 6
+// console.log(leastCommonMultiple(6, 10)); // 30
+// console.log(leastCommonMultiple(24, 26)); // 312
+
+// generatePrimes ----------- 6 -----------
+
+function generatePrimes(n) {
+    let result = [2, 3];
+
+    for (let i = 4; i < n; i++) {
+        if (n % 2 !== 0 || n % 3 !== 0) {
+            result.push(n);
+        }
+    }
+    return result;
+}
+
+console.log(generatePrimes(3)); // [2, 3, 5]
+console.log(generatePrimes(5)); // [2, 3, 5, 7, 11]
